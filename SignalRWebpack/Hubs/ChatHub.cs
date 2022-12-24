3194c5282c3ed7f34639f2d59ec404fbd76188e5
IDRISS AIT SI EL ARABI <idriss199442@gmail.com>
2022-12-24 22:25:59 +0100

    Creating a Hub that  broadcasts received messages to all connected users

---
+++ SignalRWebpack/Hubs/ChatHub.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRWebpack.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task NewMassage(long user, string message) =>
+            await Clients.All.SendAsync("messageReceived", user, message);
+    }
+}
